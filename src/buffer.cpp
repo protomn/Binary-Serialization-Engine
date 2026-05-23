025cae6c839e248d4bbc0a5dc83be39e9c0102d3
Pratham Nayak <protomn@users.noreply.github.com>
2026-05-23 20:25:54 +1000

    Buffer implementation file

---
+++ src/buffer.cpp
@@ -0,0 +1,35 @@
+#include "../include/bin_serializer/buffer.hpp"
+#include <cstring>
+#include <cstdint>
+
+namespace bin_serializer
+{
+    void Buffer::write(const void *data, size_t size)
+    {
+        const auto *bytes = static_cast<const std::uint8_t*>(data);
+
+        data_.insert(data_.end(), bytes, bytes + size);
+    }
+
+    bool Buffer::read(size_t offset, void *dest, size_t size) const
+    {
+        if(offset + size > data_.size())
+        {
+            return false;
+        }
+
+        std::memcpy(dest, data_.data() + offset, size);
+        return true;
+    }
+
+    size_t Buffer::size() const
+    {
+        return data_.size();
+    }
+
+    const uint8_t *Buffer::data() const
+    {
+        return data_.data();
+    }
+
+} //namespace bin_serializer
