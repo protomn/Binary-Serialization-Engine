84b1a73441257cc2f2f1774f8699b3c31c83ced4
Pratham Nayak <protomn@users.noreply.github.com>
2026-05-23 20:24:12 +1000

    Serializer output sink

---
+++ include/bin_serializer/buffer.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+/*
+base idea:
+    - append bytes
+    - grow storage
+    - expose size and data
+*/
+
+#include <cstdint>
+#include <cstddef>
+#include <vector> // ?? move to custom implementation later
+
+namespace bin_serializer
+{
+    class Buffer
+    {
+        private:
+            std::vector<uint8_t> data_;
+
+        public:
+
+            void write(const void *data, size_t size);
+            [[nodiscard]] bool read(size_t offset, void *dest, size_t size) const;
+
+            [[nodiscard]] const uint8_t *data() const;
+            [[nodiscard]] size_t size() const;
+    };
+} //namespace bin_serializer
